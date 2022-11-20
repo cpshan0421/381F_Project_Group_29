f8682a9861dbf42a3c6181665d08030244ba9744
cpshan0421 <118559459+cpshan0421@users.noreply.github.com>
2022-11-20 19:51:48 +0800

    Update server.js

--- S381F-Project-<Inventory Documents>/server.js
+++ S381F-Project-<Inventory Documents>/server.js
@@ -9,6 +9,11 @@ const bodyParser = require('body-parser');
 // express app
 const app = express();
 
+//HTML 
+const html = require('html');
+const url = require('url');
+const assert = require('assert');
+
 // connect to mongodb & listen for requests
 const dbURI = "mongodb+srv://admin:admin@cluster0.arpzdfb.mongodb.net/?retryWrites=true&w=majority";
 
@@ -42,6 +47,37 @@ app.use(session({
 app.use('/documents', docRoutes);
 
 
+// Create a new inventory documents
+var inventoryId = new ObjectID;
+
+ DOC["_id"] = inventoryId;
+
+        DOC['inv_id']= "";
+        DOC['inv_name']= req.fields.name;
+        DOC['inv_type']= req.fields.inv_type;
+        DOC['quantity']= req.fields.quantity;
+        DOC['photo']= req.fields.photo;
+        DOC['photo mimetype']= req.fields.photo;
+        console.log("...putting data into DOC");
+
+        var adoc ={};
+        DOC['address'] = adoc;
+        adoc['street'] = req.fields.street;
+        adoc['building'] = req.fields.building;
+        adoc['country'] = req.fields.country;
+        adoc['coord'] = req.fields.coord;
+
+        var pdoc = {};
+        if (req.files.photo && req.files.photo.size > 0 && (pdoc['mimetype'] == 'image/jpeg' || pdoc['mimetype'] == 'image/png')) {
+            fs.readFile(req.files.photo.path, (err, data) => {
+                assert.equal(err,null);
+                pdoc['title'] = req.fields.title;
+                pdoc['data'] = new Buffer.from(data).toString('base64');
+                pdoc['mimetype'] = req.files.photo.type;
+
+            });
+        }
+
 //login
 app.use((req, res, next) => {
   console.log("Checking login status");
@@ -58,13 +94,43 @@ app.use((req, res, next) => {
   }
 });
 
+//find doc with criteria
+const findDocument = (db, criteria, callback) => {
+    let cursor = db.collection('inventory').find(criteria);
+    console.log(`findDocument: ${JSON.stringify(criteria)}`);
+    cursor.toArray((err, docs)=>{
+        assert.equal(err, null);
+        console.log(`findDocument: ${docs.length}`);
+        callback(docs);
+    });
+}
+
 // routes
 app.get('/', (req, res) => {
   res.redirect('/documents');
+});
+//Delete inventory documents
+const delete inventory Document = (db, criteria, callback) => {
+    db.inventory.remove(
+       criteria,
+       (err, results) => {
+          assert.equal(err, null);
+          console.log(results);
+          callback();
+       }
+    );
+};
+app.get('/delete', (req, res)=>{
+    if(req.session.userid == req.query.owner){
+        console.log("Hello owner!");
+        handle_Delete(res, req.query);
+    }else{
+        res.status(200).render('info', {message: "You are not owner - You don't have the access right!"});
+    }
 });
 
 // 404 page
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
-
+app.listen(process.env.PORT || 8099);
