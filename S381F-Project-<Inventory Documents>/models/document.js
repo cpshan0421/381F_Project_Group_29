81279d8e0c6fce15a745c139b1b577105394fb69
cpshan0421 <118559459+cpshan0421@users.noreply.github.com>
2022-11-20 18:58:20 +0800

    Update document.js

--- S381F-Project-<Inventory Documents>/models/document.js
+++ S381F-Project-<Inventory Documents>/models/document.js
@@ -26,3 +26,20 @@ const docSchema = new Schema({
 
 const Document = mongoose.model('Document', docSchema);
 module.exports = Document;
+count DocumentsOperation extends AggregateOperation {
+  constructor(collection, query, options) {
+    const pipeline = [{ $match: query }];
+    if (typeof options.skip === 'number') {
+      pipeline.push({ $skip: options.skip });
+    }
+
+      pipeline.push({ $group: { inventory_id: 1, n: { $sum: 1 } } });
+    super(collection, pipeline, options);
+  }
+
+  execute(server, callback) {
+    super.execute(server, (err, result) => {
+      if (err) {
+        callback(err, null);
+        return;
+      }
